780768a692367edaadf32c847360d9d6d4fb0aa7
michael-schmid <m.schmid06@googlemail.com>
2014-10-02 11:22:26 +0200

    added information config module

---
+++ MyInfoWebClient/scripts/Info/infoConfig.js
@@ -0,0 +1,16 @@
+﻿
+// AMD Module for configuration
+
+define(function () {
+
+    return config = (function () {
+
+        return {
+            serviceURL: ""
+        }
+    }());
+    return {
+
+        display: getInfoForm
+    };
+});
